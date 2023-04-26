c5c149a7692e6f6d4a5948a0be8f2fbf47d02918
Bernhard Reitinger <br@rexos.org>
2023-04-26 22:20:58 +0200

    Fix problem with help parser, use vimdoc instead

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "help", "javascript", "json", "latex", "typescript", "c", "cpp", "vim", "lua", "svelte", "go" },
+    ensure_installed = { "vimdoc", "javascript", "json", "latex", "typescript", "c", "cpp", "vim", "lua", "svelte", "go" },
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
 
